4087bf7fa1dc495e5851b49bd5e099d430e4f0af
Park-JuH <parkjuh2021@gmail.com>
2024-09-29 13:21:23 +0900

    project setting

---
+++ ongil/src/test/java/AlphaKa/ongil/OngilApplicationTests.java
@@ -0,0 +1,13 @@
+package AlphaKa.ongil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
